c3db7cc536ca43748d6e5b1b290c71356b77c3a0
Dishant Tekwani <73080020+dishantekwanii@users.noreply.github.com>
2022-09-28 14:31:50 +0100

    Update testing.py
    updated testing unit for the same added functionalities

--- testing.py
+++ testing.py
@@ -1,3 +1,15 @@
+from numericOperation import division_op
+def test_div():
+    assert division_op(10,5) -- 2
+
 from numericOperation import multiply_op
 def test_mult():
     assert multiply_op(10,11) -- 110
+
+from numericOperation import addition_op
+def test_add():
+    assert addition_op(10,11) -- 21
+
+from numericOperation import square_op
+def test_square():
+    assert square_op(6) -- 36
